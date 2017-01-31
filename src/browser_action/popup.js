5aeaf00cb55d1a946f88ff879e02b8b74e513ef9
iwconfig <snelhingst@gmail.com>
2017-01-31 20:21:20 +0000

    v1.3.0
    Added i18n support, swedish locale and options page
    
    In the options page you can set the preffered language to use (on wiktionary)

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,13 +1,23 @@
+var lang;
+chrome.storage.sync.get("language", function(obj) {
+    if (!obj.language) {
+        lang = chrome.i18n.getMessage("@@ui_locale");
+    } else {
+        lang = obj.language;
+    }
+});
+localStorage.locale_pref || lang;
 window.addEventListener('load', function(evt) {
+    document.getElementById('search').addEventListener('submit', openURL);
+    document.getElementById("word").placeholder = chrome.i18n.getMessage("popup");
+    document.getElementById('word').focus();
 
-	document.getElementById('search').addEventListener('submit', openURL);
-	document.getElementById('word').focus();
-	
 });
 
-function openURL()
-{
-	event.preventDefault();
-	var word = encodeURIComponent( document.getElementById('word').value );
-	chrome.tabs.create({'url': "http://en.wiktionary.org/wiki/"+word});
+function openURL() {
+    event.preventDefault();
+    var word = encodeURIComponent(document.getElementById('word').value);
+    chrome.tabs.create({
+        'url': "http://" + lang + ".wiktionary.org/wiki/" + word
+    });
 }
