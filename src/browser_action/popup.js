64aaea212d0a936c2f0aa53475dc3a2b77c4ac6a
Thomas Roberts <thomas@edgethreesixty.com>
2017-02-14 14:47:51 +0000

    fixed the error when you first install it wont open the correct wiktionary

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,7 +1,7 @@
 var lang;
 chrome.storage.sync.get("language", function(obj) {
     if (!obj.language) {
-        lang = chrome.i18n.getMessage("@@ui_locale");
+        lang = chrome.i18n.getMessage("iso");
     } else {
         lang = obj.language;
     }
