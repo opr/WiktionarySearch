35d886972da307a47ce028060d1edeb861013eab
Thomas Roberts <celeissl@gmail.com>
2017-02-22 00:12:35 +0000

    added zip file to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var bump = require('gulp-bump');
+var zip = require('gulp-zip');
 
 gulp.task('bump:patch', bumpWithOption('patch'));
 gulp.task('bump:minor', bumpWithOption('minor'));
 gulp.task('bump:major', bumpWithOption('major'));
+gulp.task('zip:release', zipForRelease());
 
 function bumpWithOption( option ) {
     return () => {
@@ -14,6 +16,18 @@ function bumpWithOption( option ) {
 
         gulp.src(sources)
             .pipe(bump({type: option}))
+            .pipe(gulp.dest('./'));
+    }
+}
+
+
+function zipForRelease() {
+    return () => {
+        let sources = [
+          'src/**'
+        ];
+        gulp.src(sources)
+            .pipe(zip('wiktionarysearch.zip'))
             .pipe(gulp.dest('./'));
     }
 }
